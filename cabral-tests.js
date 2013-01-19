e741bc4e8839628aa917adb7b92c1272db4a0504
fabio.oshiro <fabio.oshiro@gmail.com>
2013-01-19 17:51:20 -0200

    gh page manual

---
+++ cabral-tests.js
@@ -0,0 +1,76 @@
+
+// this is the frame that cabral will manipulate
+cabral.setFrameName('escotilha');
+
+describe("cabral should", function(){
+	
+	it("navigate to an url", function(){
+		var done, win;
+		runs(function(){
+			cabral.navigateTo('bar.html', function(_win){
+				console.log("Page bar is loaded");
+				win = _win;
+				done = true;
+			});
+		});
+
+		waitsFor(function() {
+			return done;
+		}, "The Value should be incremented", 750);
+
+		runs(function(){
+			expect(win.document.title).toBe('Cabral Test Page - Bar');
+		});
+	});
+
+	it("click link by text", function(){
+		var done, win;
+		runs(function(){
+			cabral.navigateTo('bar.html', function(){
+				console.log("Page bar is loaded");
+
+				console.log("Now click in link foo...");
+				cabral.clickLink('foo', function(_win){
+					console.log("Page foo is loaded");
+
+					win = _win;
+					done = true;
+				});
+			});
+		});
+
+		waitsFor(function() {
+			return done;
+		}, "The Value should be incremented", 1000);
+
+		runs(function(){
+			expect(win.document.title).toBe('Cabral Test Page - Foo');
+		});
+	});
+
+	it("throw an exception if link isnt present", function(){
+		var done, win;
+		runs(function(){
+			cabral.navigateTo('bar.html', function(_win){
+				win = _win;
+				console.log("Page bar is loaded");
+
+				console.log("Now link with text is not found...");
+				expect(function(){
+					cabral.clickLink('a 404 page', function(_win){});
+				}).toThrow(new Error("Link not found with text 'a 404 page'."));
+				
+				done = true
+			});
+		});
+
+		waitsFor(function() {
+			return done;
+		}, "The Value should be incremented", 1000);
+
+		runs(function(){
+			expect(win.document.title).toBe('Cabral Test Page - Bar');
+		});
+	});
+
+});
---
+++ cabral.js
@@ -0,0 +1,194 @@
+
+
+/**
+ * Cabral the Navigator
+ * @constructor
+ */
+var Cabral = function(){
+
+	var _config = typeof(config) == 'undefined' ? {contextPath: ''} : config;
+
+	var win = 'appFrame';
+	var self = this;
+	function onPageChange(){
+		getWin().confirm = function(){return true};
+	}
+
+	/**
+	   @param {string} frameName - The frame or iframe name for cabral manipulation
+	 */
+	this.setFrameName = function(anFrameName){
+		win = anFrameName;
+	};
+
+	function getWin(){
+		if(window.parent.window.opener){
+			return window.parent.window.opener;
+		}else{
+			return window[win];
+		}
+	}
+
+	function callCallBack(callback, args){
+		var tid = setInterval( function () {
+		    if ( getWin().document.readyState !== 'complete' ) return;
+		    clearInterval( tid );
+
+		    if(typeof(getWin()['$']) != 'undefined'){
+				callback(getWin().$, args);	
+			}else{
+				console.log('No jQuery found...');
+				callback(getWin(), args);
+			}
+		}, 100 );
+	}
+
+	/**
+	 * ref http://blog.stchur.com/2010/01/15/programmatically-clicking-a-link-in-javascript/
+	 */
+	function actuateLink(link){
+		if(!link){
+			throw 'Undefined link';
+		}
+		var allowDefaultAction = true;
+	      
+	   	if (link.click){
+			link.click();
+			return;
+		}else if (getWin().document.createEvent){
+			var e = getWin().document.createEvent('MouseEvents');
+			e.initEvent(
+				'click'     // event type
+				,true      // can bubble?
+				,true      // cancelable?
+			);
+			allowDefaultAction = link.dispatchEvent(e);
+		}
+		if (allowDefaultAction){
+			var f = getWin().document.createElement('form');
+			f.action = link.href;
+			getWin().document.body.appendChild(f);
+			f.submit();
+		}
+	}
+
+	/**
+	 * Intercepted by TomateGrailsPlugin.groovy
+	 */
+	function defFillFile(el, fileName){
+		var name = el.getAttribute('name')
+		var parent = el.parentNode;
+		var input = getWin().document.createElement('input');
+		input.setAttribute('name', 'tomate_file_' + name);
+		input.setAttribute('value', fileName);
+		input.setAttribute('type', 'hidden');
+		parent.appendChild(input);
+	}
+
+	var regexComparator = function(uri){
+		return uri.exec(getWin().location.href);
+	};
+
+	var endsWithComparator = function(uri){
+		var local = getWin().location.href.substr(uri.length * -1);
+		return local == uri;
+	};
+
+	var waitForUrl = function(uri, comparator, callback){
+		var loadListener = function(){
+			var t;
+			if(t = comparator(uri)){
+				onPageChange();
+				callCallBack(callback, t);
+			}else{
+				setTimeout(loadListener, 250);
+			}
+		}
+		setTimeout(loadListener, 250);
+	};
+
+	/**
+	 * @param {string} uri - the page to go. eg. '/book/create'
+	 * @param {function} callback - callback function
+	 * @example 
+	 * cabral.navigateTo('/book/create', function($){
+	 *     // $ is a jQuery if present
+	 *     // or is a window
+	 * });
+	 */
+	this.navigateTo = function(uri, callback){
+		if(endsWithComparator(uri)){
+			callCallBack(callback, true);
+		}else{
+			getWin().location.href = _config.contextPath + uri;
+			waitForUrl(uri, endsWithComparator, callback);	
+		}
+	};
+
+	/**
+	 * Wait for a page to load.
+	 * @param {string or regex} uri - the URI to wait for
+	 * @param {function} callback - will be called when page load
+	 * @example
+	 * &lt;form action="/book/save" />
+	 *     &lt;input type="submit" id="create" />
+	 * &lt;/form>
+	 *
+	 * $('#create').click();
+	 * cabral.waitFor('/book/save', function($){
+	 *     // ...
+	 * });
+	 */
+	this.waitFor = function(uri, callback){
+		waitForUrl(uri, (typeof(uri) == 'string' ? endsWithComparator : regexComparator), callback);
+	};
+
+	/**
+     * Simulate a click &lt;a href="somePage.html">click here&lt;/a>
+	 * @param {string} linkText - eg. click here
+	 * @param {function} callback - Fired on page load
+	 * @example
+	 * cabral.clickLink('click here', function($){
+     *     // evaluate your page
+	 *     var message = $('.message').text();
+	 * });
+	 */
+	this.clickLink = function(){
+		if(arguments.length == 2){
+			var links = getWin().document.getElementsByTagName('a');
+			for(var i = 0; i < links.length; i++){
+				if(links[i].innerHTML == arguments[0]){
+					actuateLink(links[i]);
+					self.waitFor(links[i].getAttribute('href'), arguments[1]);
+					return;
+				}
+			}
+			console.log("Link not found with text '" + arguments[0] + "'.");
+			throw new Error("Link not found with text '" + arguments[0] + "'.");
+		}else if(arguments.length == 1){
+			actuateLink(arguments[0]);
+		}
+	};
+
+	/**
+	 * Pretend to fill a input type file.<br>
+	 * The javascript can't change a value of an input with type "file".<br>
+	 * So Cabral creates a hidden field to send your file name value.<br>
+	 * This value will be intercepted by TomateGrailsPlugin.groovy 
+	 * with a modified version of request.getFile('yourfile.txt') 
+	 * that get that file from &lt;yourapp>/test/resources/yourfile.txt
+	 * @param {element} el - input file tag
+	 * @param {string} fileName - name of your file
+	 * @example
+	 * &lt;input type="file" id="myfile" />
+	 *
+	 * var el = $('#myfile')[0];
+	 * cabral.fillFile(el, 'yourfile.txt') ;
+	 */
+	this.fillFile = function(el, fileName){
+		defFillFile(el, fileName);
+	}
+}
+
+var cabral = new Cabral();
+
